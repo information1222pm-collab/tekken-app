60263cc2516351e6ae1456541e140a7f4fe3625f
information1222pm-collab <information1222pm@gmail.com>
2025-09-15 09:40:41 -0400

    Update vite.config.js

--- apps/web/vite.config.js
+++ apps/web/vite.config.js
@@ -1,7 +1,4 @@
+// apps/web/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  build: { outDir: 'dist' }
-})
+export default defineConfig({ plugins:[react()], build:{ outDir:'dist' } })
