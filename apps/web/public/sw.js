f32b3d82d27ca7a1b415e284dec51c79b187ee5d
information1222pm-collab <information1222pm@gmail.com>
2025-09-15 05:15:16 -0400

    Update sw.js

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('tm-v1').then(c => c.addAll(['/', '/index.html'])));
+  e.waitUntil(caches.open('tm-v1').then(c => c.addAll(['/', '/index.html'])))
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
